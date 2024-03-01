f28253a7c2c46f2ce4497fdf90ee4abf8985ede9
Aliaks <aliaksmarchuk@gmail.com>
2024-03-01 23:59:09 +0100

    adding GUI components

--- WeatherApp/src/GUI.java
+++ WeatherApp/src/GUI.java
@@ -37,7 +37,6 @@ public class GUI extends JFrame {
     public void addComponents(){
 
         JTextField searchField = new JTextField();
-
         searchField.setBounds(20, 15, 350, 45);
         searchField.setFont(new Font("SansSerif", Font.BOLD, 24));
         searchField.setHorizontalAlignment(JTextField.CENTER);
@@ -45,10 +44,39 @@ public class GUI extends JFrame {
         add(searchField);
 
         JButton searchButton = new JButton(getImage("src/images/search.png"));
-
         searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         searchButton.setBounds(380, 13, 47, 45);
 
         add(searchButton);
+
+        JLabel weatherImage = new JLabel(getImage("src/images/clouds.png"));
+        weatherImage.setBounds(0,125, 450, 217);
+
+        add(weatherImage);
+
+        JLabel temperatureText = new JLabel("20 C");
+        temperatureText.setBounds(0,350, 450, 54);
+        temperatureText.setFont(new Font("SansSerif", Font.BOLD, 40));
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+
+        add(temperatureText);
+
+        JLabel weatherCondition = new JLabel("Cloudy");
+        weatherCondition.setBounds(0, 405, 450, 36);
+        weatherCondition.setFont(new Font("SansSerif", Font.BOLD, 30));
+        weatherCondition.setHorizontalAlignment(SwingConstants.CENTER);
+
+        add(weatherCondition);
+
+        JLabel humidityImage = new JLabel(getImage("src/images/humidity.png"));
+        humidityImage.setBounds(15, 500, 74, 66);
+
+        add(humidityImage);
+
+        JLabel humidityText = new JLabel("Humidity\n100%");
+        humidityText.setBounds(90, 500, 85, 55);
+        humidityText.setFont(new Font("SansSerif", Font.BOLD, 16));
+
+        add(humidityText);
     }
 }
